e9db6c6f2e7ee7e788521446948e016f5b2d083e
Vincent <vlagria3@gmail.com>
2022-09-11 19:20:50 +0800

    changing html structure

---
+++ resources/views/portfolio/welcome.blade.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <div class="header-text">
+                <h2>hi <span>,</span> i am <br> vincent <br> Lagria <span>.</span>   </h2>
+                <p>Software and web developer</p>
+                <a href="{{ asset('assets/download/browney.txt') }}" download>download resume</a>
+            </div><!--/.header-text-->
+        </div><!--/.col-->
+    </div><!-- /.row-->
+</div><!-- /.container-->
